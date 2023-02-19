b603a3a56622045fd826b6c43e069c817ff6f53b
gabrial-dagon <asmitul@users.noreply.github.com>
2023-02-19 23:13:34 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pdf2png
+
 convert pdf file to png
+
+# convert sample.pdf to image
---
+++ main.py
@@ -0,0 +1,23 @@
+# pip install pdf2image
+# poppler
+
+
+from pdf2image import convert_from_path
+from pathlib import Path
+
+
+def main() -> None:
+    BASE_DIR = Path.cwd()
+    print(BASE_DIR)
+    pdf_file = f"{BASE_DIR}\sample.pdf"
+    print(pdf_file)
+    images = convert_from_path(pdf_file)
+
+    for i in range(len(images)):
+
+        # Save pages as images in the pdf
+        images[i].save('sample' + str(i) + '.png', 'PNG')
+
+
+if __name__ == "__main__":
+    main()
