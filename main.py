047701159468cc07a92fa3594c378b78af5774f0
gabrial-dagon <asmitul@users.noreply.github.com>
2023-03-01 13:25:10 +0800

    --

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ from pathlib import Path
 def main() -> None:
     BASE_DIR = Path.cwd()
     print(BASE_DIR)
-    pdf_file = f"{BASE_DIR}\sample.pdf"
+    pdf_file = f"{BASE_DIR}\sample_1-39.pdf"
     print(pdf_file)
     images = convert_from_path(pdf_file)
 
     for i in range(len(images)):
 
         # Save pages as images in the pdf
-        images[i].save('sample' + str(i) + '.png', 'PNG')
+        images[i].save('sample_1-39-' + str(i) + '.png', 'PNG')
 
 
 if __name__ == "__main__":
